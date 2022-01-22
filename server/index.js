3c0a6ce84e27c38f3709c1582ccb38cc04c79040
alceil <ashishshajisrampickal@gmail.com>
2022-01-22 15:46:21 +0530

    added index code

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { v4 as uuid } from 'uuid';
+
+import Connection from './database/db.js';
+import DefaultData from './default.js';
+import Routes from './routes/route.js';
+
+
+dotenv.config();
+const app = express();
+
+const PORT = 8000;
+
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
+
+Connection(username, password);
+
+app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
+DefaultData();
+
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use('/', Routes);
