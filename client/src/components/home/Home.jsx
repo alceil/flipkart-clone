f32623dd300a6ff49da304f1857c91043bd87984
alceil <ashishshajisrampickal@gmail.com>
2022-01-22 12:31:22 +0530

    Added Slider banner

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,11 +1,51 @@
 import React from 'react';
-// import Banner from './Banner';
+import Slide from './Slide';
+import { Box, makeStyles } from '@material-ui/core';
+import { products } from '../../constants/product';
+import Banner from './Banner';
 import NavBar from './NavBar';
+
+const useStyle = makeStyles({
+  component: {
+      padding: 10,
+      background: '#F2F2F2'
+  }
+})
+
 const Home = () => {
+  const classes = useStyle();
   return(
       <div>
         <NavBar/>
-        helllo borld
+        <Box className={classes.component}>
+                <Banner />
+                {/* <MidSlide products={products} /> */}
+                {/* <MidSection /> */}
+                <Slide
+                    data={products} 
+                    title='Discounts for You'
+                    timer={false} 
+                    multi={true} 
+                />
+                <Slide
+                    data={products} 
+                    title='Suggested Items'
+                    timer={false} 
+                    multi={true} 
+                />
+                <Slide
+                    data={products} 
+                    title='Top Selection'
+                    timer={false} 
+                    multi={true} 
+                />
+                <Slide
+                    data={products} 
+                    title='Recommended Items'
+                    timer={false} 
+                    multi={true} 
+                />
+            </Box>
 {/* <Banner/> */}
       </div>
       )
